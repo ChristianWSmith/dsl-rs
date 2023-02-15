7ec54774cd98363094d457ddac371b726597b806
Christian Smith <smith.christian.william@gmail.com>
2023-02-15 13:05:29 -0500

    client excepts cli input

--- dsl/src/bin/client.rs
+++ dsl/src/bin/client.rs
@@ -1,8 +1,11 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0);
+    let command = args.join(" ");
     let conn = dbus::blocking::Connection::new_session()?;
     let proxy = conn.with_proxy("com.ChristianWSmith.dsl", "/swaycommand", std::time::Duration::from_millis(5000));
-    let (reply,): (String,) = proxy.method_call("com.ChristianWSmith.dsl", "SwayCommand", ("focus right",))?;
+    let (reply,): (String,) = proxy.method_call("com.ChristianWSmith.dsl", "SwayCommand", (command,))?;
     println!("{}", reply);
     Ok(())
 }
