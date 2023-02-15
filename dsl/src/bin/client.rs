b06f7d6a371b20917202b56ebc6065d6d3c52d4d
Christian Smith <smith.christian.william@gmail.com>
2023-02-15 15:16:47 -0500

    client silent

--- dsl/src/bin/client.rs
+++ dsl/src/bin/client.rs
@@ -6,6 +6,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = dbus::blocking::Connection::new_session()?;
     let proxy = conn.with_proxy("com.ChristianWSmith.dsl", "/swaycommand", std::time::Duration::from_millis(5000));
     let (reply,): (String,) = proxy.method_call("com.ChristianWSmith.dsl", "SwayCommand", (command,))?;
-    println!("{}", reply);
     Ok(())
 }
