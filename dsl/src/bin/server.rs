926f6838f56fd6d9e2ff909ba72949cd382fc651
Christian Smith <smith.christian.william@gmail.com>
2023-02-15 15:11:49 -0500

    reuse sway connection

--- dsl/src/bin/server.rs
+++ dsl/src/bin/server.rs
@@ -1,12 +1,16 @@
+#[macro_use]
+extern crate lazy_static;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    lazy_static! {
+        static ref SWAY: std::sync::Mutex<swayipc::Connection> = std::sync::Mutex::new(swayipc::Connection::new().unwrap());
+    }
     let c = dbus::blocking::Connection::new_session()?;
     c.request_name("com.ChristianWSmith.dsl", false, true, false)?;
     let mut cr = dbus_crossroads::Crossroads::new();
     let token = cr.register("com.ChristianWSmith.dsl", |b| {
         b.method("SwayCommand", ("command",), ("reply",), |_, _, (command,): (String,)| {
-            // TODO: how can we connect once and reuse this connection, rather than reconnecting each time?
-            let mut sway_connection = swayipc::Connection::new().unwrap();
-            sway_connection.run_command(&command);
+            SWAY.lock().unwrap().run_command(&command);
             Ok((format!("{}", &command),))
         });
     });
