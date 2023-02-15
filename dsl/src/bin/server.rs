e86c9f310b4e294931dd627db857a102e7cd3678
Christian Smith <smith.christian.william@gmail.com>
2023-02-15 13:06:20 -0500

    TODO

--- dsl/src/bin/server.rs
+++ dsl/src/bin/server.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cr = dbus_crossroads::Crossroads::new();
     let token = cr.register("com.ChristianWSmith.dsl", |b| {
         b.method("SwayCommand", ("command",), ("reply",), |_, _, (command,): (String,)| {
+            // TODO: how can we connect once and reuse this connection, rather than reconnecting each time?
             let mut sway_connection = swayipc::Connection::new().unwrap();
             sway_connection.run_command(&command);
             Ok((format!("{}", &command),))
