1516118383611eb6ef084325eb9daf7e4521aebc
Christian Smith <smith.christian.william@gmail.com>
2023-02-16 13:46:43 -0500

    layout now doing splits

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,29 +1,79 @@
-fn process_layout(sway: &swayipc::Connection) {
-    println!("process_layout");
+fn get_leaf_containers(node: &swayipc::Node) -> Vec<&swayipc::Node> {
+    let mut out: Vec<&swayipc::Node> = vec![];
+    if node.nodes.len() == 0 {
+        if node.node_type == swayipc::NodeType::Con {
+            out.push(node);
+        }
+    } else {
+        for child in &node.nodes {
+            out.extend(get_leaf_containers(&child));
+        }
+    }
+    out
 }
 
-fn process_move(sway: &swayipc::Connection, tokens: Vec<&str>) {
+fn enforce_layout_workspace(workspace: swayipc::Node) -> Vec<String> {
+    let mut out: Vec<String> = vec![];
+    let leaves = get_leaf_containers(&workspace);
+    if leaves.len() == 1 {
+        let window = *leaves.get(0).unwrap();
+        if !workspace.nodes.contains(window) {
+            out.push(format!("[con_id={}] move up; ", window.id));
+        }
+        out.push(format!("[con_id={}] splith; ", window.id));
+    } else if leaves.len() == 2 {
+        let left = *leaves.get(0).unwrap();
+        let right = *leaves.get(1).unwrap();
+        out.push(format!("[con_id={}] splitv; ", left.id));
+        out.push(format!("[con_id={}] splitv; ", right.id));
+    }
+    out
+}
+
+fn enforce_layout(head: swayipc::Node) -> Vec<String> {
+    let mut out: Vec<String> = vec![];
+    if head.node_type == swayipc::NodeType::Workspace {
+        out.extend(enforce_layout_workspace(head));
+    } else {
+        for node in head.nodes {
+            out.extend(enforce_layout(node));
+        }
+    }
+    out
+}
+
+fn process_layout(sway: &mut swayipc::Connection) {
+    let tree = sway.get_tree().unwrap();
+    let sway_commands = enforce_layout(tree);
+    println!("{:?}", sway_commands);
+    sway.run_command(sway_commands.concat());
+}
+
+fn process_move(sway: &mut swayipc::Connection, tokens: Vec<&str>) {
     println!("process_move {:?}", tokens);
+    let tree = sway.get_tree().unwrap();
 }
 
-fn process_move_to_workspace(sway: &swayipc::Connection, tokens: Vec<&str>) {
+fn process_move_to_workspace(sway: &mut swayipc::Connection, tokens: Vec<&str>) {
     println!("process_move_to_workspace {:?}", tokens);
+    let tree = sway.get_tree().unwrap();
 }
 
-fn process_kill(sway: &swayipc::Connection) {
+fn process_kill(sway: &mut swayipc::Connection) {
     println!("process_kill");
+    let tree = sway.get_tree().unwrap();
 }
 
 fn command_processor(command_receiver: async_priority_channel::Receiver<String, usize>) {
-    let sway: swayipc::Connection = swayipc::Connection::new().unwrap();
+    let mut sway: swayipc::Connection = swayipc::Connection::new().unwrap();
     loop {
         let command: String = sync_recv(&command_receiver);
         let mut tokens: Vec<&str> = command.split_whitespace().collect();
         match tokens.remove(0) {
-            dsl::constants::CMD_LAYOUT => process_layout(&sway),
-            dsl::constants::CMD_MOVE => process_move(&sway, tokens),
-            dsl::constants::CMD_MOVE_TO_WORKSPACE => process_move_to_workspace(&sway, tokens),
-            dsl::constants::CMD_KILL => process_kill(&sway),
+            dsl::constants::CMD_LAYOUT => process_layout(&mut sway),
+            dsl::constants::CMD_MOVE => process_move(&mut sway, tokens),
+            dsl::constants::CMD_MOVE_TO_WORKSPACE => process_move_to_workspace(&mut sway, tokens),
+            dsl::constants::CMD_KILL => process_kill(&mut sway),
             _ => continue,
         }
     }
