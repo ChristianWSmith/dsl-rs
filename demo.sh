413391cfbffb7b0a91ac5c89b69a044d0df0762b
Christian Smith <smith.christian.william@gmail.com>
2023-02-17 14:00:03 -0500

    add a 0.1 second sleep between footclients in demo

--- demo.sh
+++ demo.sh
@@ -10,10 +10,15 @@ workspace=$(swaymsg -t get_workspaces | jq '.[] | select(.focused==true) | .name
 swaymsg workspace demo
 
 footclient fish -c "figlet A | lolcat -F 0.4; read -p ''" & pidA=$!
+sleep 0.1
 footclient fish -c "figlet B | lolcat -F 0.4; read -p ''" & pidB=$!
+sleep 0.1
 footclient fish -c "figlet C | lolcat -F 0.4; read -p ''" & pidC=$!
+sleep 0.1
 footclient fish -c "figlet D | lolcat -F 0.4; read -p ''" & picD=$!
+sleep 0.1
 footclient fish -c "figlet E | lolcat -F 0.4; read -p ''" & picE=$!
+sleep 0.1
 
 wait $pidA
 wait $pidB
